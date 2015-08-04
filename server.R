e40942e77a14a50c275e4cf3a699cd87e3187c8f
Jonathan Marshall <jmarshall@xbmc.org>
2015-08-04 19:54:31 +1200

    tidy up the shiny app

--- server.R
+++ server.R
@@ -8,7 +8,7 @@ if (!create_database()) {
 
 shinyServer(function(input, output, session) {
 
-  v <- reactiveValues(scores = read_samples())
+  v <- reactiveValues(scores = read_scores())
 
   # make the submit button do something useful
   observeEvent(input$submit, {
@@ -29,27 +29,28 @@ shinyServer(function(input, output, session) {
 
   # plot on the left shows actual data
   output$score <- renderPlot( {
-    par(mar=c(3,3,3,0))
-    hist(v$scores$score, main="Score", xaxs="i")
-    abline(v=input$score, col="red")
+    par(mar=c(4,4,3,0))
+    hist(v$scores$score, main="Score", xlim=range(v$scores$score, input$score, na.rm=TRUE), xaxs="i", col="grey60", xlab="")
+    abline(v=input$score, lwd=2)
   } )
 
   output$bonus <- renderPlot( {
-    par(mar=c(3,3,3,0))
-    hist(v$scores$bonus, main="Bonus", xaxs="i")
-    abline(v=input$bonus, col="red")
+    par(mar=c(4,4,3,0))
+    hist(v$scores$bonus, xlim=range(v$scores$bonus, input$bonus, na.rm=TRUE), main="Bonus", xaxs="i", col="pink", xlab="")
+    abline(v=input$bonus, lwd=2)
   } )
 
   output$total <- renderPlot( {
-    par(mar=c(3,3,3,0))
-    hist(v$scores$bonus + v$scores$score, main="Total score", xaxs="i")
-    abline(v=input$bonus+input$score, col="red")
+    par(mar=c(4,4,3,0))
+    xlim <- range(v$scores$bonus + v$scores$score, input$bonus+input$score, na.rm=TRUE)
+    hist(v$scores$bonus + v$scores$score, xlim=xlim, main="Total score", xaxs="i", col="lightblue", xlab="")
+    abline(v=input$bonus+input$score, lwd=2)
   } )
 
   output$score_bonus <- renderPlot( {
-    par(mar=c(3,3,3,0))
-    plot(score ~ bonus, data=v$scores, pch=19, col="grey50")
-    points(input$bonus, input$score, col="red")
+    par(mar=c(4,4,3,0))
+    plot(score ~ bonus, data=rbind(v$scores, c(input$score, input$bonus)), pch=19, col="grey50", xlab="Bonus", ylab="Score", main="Score versus Bonus", bty="n")
+    points(input$bonus, input$score, col="red", pch=19)
   } )
 
 })
--- ui.R
+++ ui.R
@@ -1,6 +1,7 @@
 library(shiny)
 
 shinyUI(fluidPage(
+  titlePanel("Quizzing with Confidence"),
   fluidRow(
     column(width=3, plotOutput("score")),
     column(width=3, plotOutput("bonus")),
@@ -8,8 +9,10 @@ shinyUI(fluidPage(
     column(width=3, plotOutput("score_bonus"))
   ),
   wellPanel(
-    numericInput("score", "Score", value=0, min=-250, max=250, step=1),
-    numericInput("bonus", "Bonus", value=0, min=0, max=125, step=1)
+    fluidRow(
+      column(width=3, numericInput("score", "Score", value=0, min=-250, max=250, step=1)),
+      column(width=3, numericInput("bonus", "Bonus", value=0, min=0, max=125, step=1)),
+      column(width=3, actionButton("submit", "Submit"))
     )
   )
-)
+))
